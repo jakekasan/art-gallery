9d1e69ca8476bbdd71f3a00f126a990a8ebce1eb
jakekasan <jake.kasan@gmail.com>
2018-09-26 16:55:34 +0100

    added rendering engine, changed views folder and added static folder

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const config = require("./config/index");
+const config = require("./config/index")();
+const homeController = require("./controllers/home.controller");
 
-app.set("view-engine","ejs");
+app.set("view engine","ejs");
+
+app.set("views","./views/templates");
+
+app.use(express.static("public"));
+
+app.all("/",(req,res,next) => {
+    homeController.run(req,res,next);
+});
+
+app.listen(config.server.port,() => {
+    console.log("Server running at port",config.server.port);
+});
